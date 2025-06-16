331cabd85e34577f5f9589d1b9b6161ef54672fe
BYTEGUARDIAN14 <jmmunnerahmed@gmail.com>
2025-06-16 11:04:56 +0530

    Update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BYTEGUARDIAN-PORTFOLIO-main/',
+  base: '/my-portfolio/',
   server: {
     host: "::",
     port: 8080,
